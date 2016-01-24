5d56b108e2b113c0f9600636796d1fd82cd100fa
Jeronimo Garcia <jgarcia@jeronimos-air.home>
2016-01-24 16:20:07 +0000

    moving to offlineimap

--- addr.sh
+++ addr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-cat /Users/jgarcia/Mail/bodies/cache/imaps:ipsoft%2Fjegarcia@usmail.ipsoft.com:993/INBOX/* | egrep -io "\b$1[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b" | sort | uniq
+cat /Users/jgarcia/OfflineMail/INBOX/cur/* | egrep -io "\b$1[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b" | sort | uniq
 
